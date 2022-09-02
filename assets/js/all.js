01f28b2a6d41c00e70d472d7f2dd1869054abb0d
Ryan Chiang <wsad71155@gmail.com>
2022-09-02 23:58:22 +0800

    Update 2022-09-02T15:58:18.920Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,11 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+$(".search-btn").click(function (e) {
+  e.preventDefault();
+  $(".search").toggleClass("active");
+  $(".searchBar").toggleClass("active");
+  $(".search-btn-back").toggleClass("active");
+  $(".logo").toggleClass("hidden");
+  $(".hamberger").toggleClass("hidden");
 });
 //# sourceMappingURL=all.js.map
